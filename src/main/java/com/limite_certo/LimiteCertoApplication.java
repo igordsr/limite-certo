aff38dabea9165b0d261f8abd8f8f393f9a79287
Igor <37842175+igordsr@users.noreply.github.com>
2024-07-30 20:14:34 -0300

    INIT-PROJETO

---
+++ src/main/java/com/limite_certo/LimiteCertoApplication.java
@@ -0,0 +1,13 @@
+package com.limite_certo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimiteCertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimiteCertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limite_certo/LimiteCertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limite_certo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimiteCertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
